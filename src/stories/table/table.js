99ddff10bb2443359ba466e9dc4fa77227dd0663
Vadim Sartakov <contact.vadimsartakov@gmail.com>
2019-08-23 15:39:10 +0500

    Refactor test table

--- src/stories/table/table.js
+++ src/stories/table/table.js
@@ -13,70 +13,63 @@ import {
 } from '../../components/Table';
 import './table.sass';
 
-const columns = [
-  {
-    title: 'Column 1',
-    key: 'fieldOne'
-  },
-  {
-    title: 'Column 2',
-    key: 'fieldTwo'
-  },
-  {
-    title: 'Column 3',
-    key: 'fieldThree'
-  },
-  {
-    title: 'Column 4',
-    key: 'fieldFour'
-  }
-];
-
-const values = new Array(20).fill(1).map((item, index) => {
+const createColumns = count => new Array(count).fill(1).map((item, index) => {
   return {
-    fieldOne: 'Field one: ' + index,
-    fieldTwo: 'Field two: ' + index,
-    fieldThree: 'Field three: ' + index,
-    fieldFour: 'Field four: ' + index,
+    title: `Column ${index}`,
+    key: `field${index}`
   };
 });
 
-const TestTable = props => (
-  <Table
-      className="table"
-      defaultRowHeight={16}
-      defaultColumnWidth={300}
-      {...props}>
-    <TableHeader>
-      <TableRow index={0}>
-        {columns.map((column, index) => (
-          <TableHeaderCell key={index} index={index}>
-            <TableCellValue>
-              {column.title}
-            </TableCellValue>
-            <TableColumnResizer index={index} className="resizer column-resizer" />
-            <TableRowResizer className="resizer row-resizer" />
-          </TableHeaderCell>
-        ))}
-      </TableRow>
-    </TableHeader>
-    
-    <TableBody>
-      {values.map((value, rowIndex) => (
-        <TableRow key={rowIndex} index={rowIndex + 1}>
-          {columns.map((column, columnIndex) => (
-            <TableCell key={columnIndex}>
+const createValues = (columns, count) => new Array(count).fill(1).map((item, valueIndex) => {
+  return columns.reduce((acc, column, columnIndex) => {
+    return {
+      ...acc,
+      [column.key]: `Value ${valueIndex} - ${columnIndex}`
+    };
+  }, {});
+});
+
+const TestTable = ({ columnsCount, valuesCount, ...props }) => {
+  const columns = createColumns(columnsCount);
+  const values = createValues(columns, valuesCount);
+  return (
+    <Table
+        className="table"
+        defaultRowHeight={16}
+        defaultColumnWidth={300}
+        {...props}>
+      <TableHeader>
+        <TableRow index={0}>
+          {columns.map((column, index) => (
+            <TableHeaderCell key={index} index={index}>
               <TableCellValue>
-                {value[column.key]}
+                {column.title}
               </TableCellValue>
+              <TableColumnResizer index={index} className="resizer column-resizer" />
               <TableRowResizer className="resizer row-resizer" />
-            </TableCell>
+            </TableHeaderCell>
           ))}
         </TableRow>
-      ))}
-    </TableBody>
-  </Table>
-);
+      </TableHeader>
+      
+      <TableBody>
+        {values.map((value, rowIndex) => (
+          <TableRow key={rowIndex} index={rowIndex + 1}>
+            {columns.map((column, columnIndex) => (
+              <TableCell key={columnIndex}>
+                <TableCellValue>
+                  {value[column.key]}
+                </TableCellValue>
+                <TableRowResizer className="resizer row-resizer" />
+              </TableCell>
+            ))}
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+};
 
 storiesOf('Table', module)
-  .add('default', () => <TestTable />);
+  .add('default', () => <TestTable columnsCount={6} valuesCount={20} />)
+  .add('fixed rows', () => <TestTable columnsCount={6} valuesCount={20} fixRows={2} />);
