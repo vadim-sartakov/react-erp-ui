567dd858dc8b4cb60b841727c73e3134ccfea316
Vadim Sartakov <contact.vadimsartakov@gmail.com>
2019-08-29 16:02:25 +0500

    Finalize static scroller api

--- src/components/StaticScroller/StaticScroller.js
+++ src/components/StaticScroller/StaticScroller.js
@@ -1,13 +1,13 @@
-import React, { useRef } from 'react';
+import React, { useState, useRef, useCallback } from 'react';
 import PropTypes from 'prop-types';
 
-const StaticScroller = ({
+const StaticScroller1 = ({
   ...props
 }) => {
   return <div {...props} />
 };
 
-StaticScroller.propTypes = {
+StaticScroller1.propTypes = {
   defaultColumnWidth: PropTypes.number.isRequired,
   defaultRowHeight: PropTypes.number.isRequired,
   rows: PropTypes.arrayOf(PropTypes.shape({
@@ -20,10 +20,110 @@ StaticScroller.propTypes = {
   })).isRequired
 };
 
-const rowsPerPage = 100;
-const columnsPerPage = 50;
+const exampleRows = {
+  paddings: {
+    start: 20,
+    end: 50
+  },
+  page: 5,
+  rows: [ // Already shrinked visible rows meta
+    {
+      size: 10,
+      expanded: true,
+      paddings: {/* ... */},
+      rows: [
+        // ...
+      ]
+    }
+  ]
+  // ...
+};
 
-const useBufferedPages = (page, value) => {
+const exampleColumns = {
+  paddings: {
+    start: 10,
+    end: 20
+  },
+  rows: [
+    {
+      size: 20,
+      expanded: true,
+      paddings: {/* ... */},
+      columns: [
+        // ...
+      ]
+    }
+  ]
+  // ...
+};
+
+// Second column expanded
+// Third row expanded
+const exampleValue = [ // Rows
+  { //Row 0
+    columns: [ // Columns
+      { // Column 0 (Cell 0-0)
+        value: 'Value 0 - 0'
+      },
+      { // Column 1 (Cell 0-1)
+        value: 'Value 0 - 1',
+        children: [ // Expanded columns
+          { // Column 0 (Cell 0-1-0)
+            value: 'Value 0 - 1 - 0'
+          },
+          { // Column 1 (Cell 0-1-1)
+            value: 'Value 0 - 1 - 1'
+          }
+        ]
+      }
+    ]
+  },
+  { // Row 1
+    columns: [ // Columns
+      { // Column 0 (Cell 1-0)
+        value: 'Value 1 - 0'
+      },
+      { // Column 1 (Cell 1-1)
+        value: 'Value 1 - 1',
+        children: [ // Expanded columns
+          { // Column 0 (Cell 1-1-0)
+            value: 'Value 1 - 1 - 0'
+          },
+          { // Column 1 (Cell 1-1-1)
+            value: 'Value 1 - 1 - 1'
+          }
+        ]
+      }
+    ]
+  },
+  { //Row 2
+    columns: [ // Columns
+      { // Column 0 (Cell 2-0)
+        value: 'Value 2 - 0'
+      },
+      { // Column 0 (Cell 2-1)
+        value: 'Value 2 - 1',
+        children: [ // Expanded columns
+          { // Column 0 (Cell 2-1-0)
+            value: 'Value 2 - 1 - 0'
+          },
+          { // Column 1 (Cell 2-1-1)
+            value: 'Value 2 - 1 - 1'
+          }
+        ]
+      }
+    ],
+    children: [
+      {
+        columns: [
+          // ... the same column structure as for previous rows
+        ]
+      }
+    ]
+  },
+];
+
+const useBufferedPages = (page, value, itemsPerPage, loadPage) => {
 
   // useEffect here will make it laggy
   // So using local variables
@@ -34,7 +134,7 @@ const useBufferedPages = (page, value) => {
 
   const cachedPage = getCacheValue(page);
   if (!cachedPage) {
-    const newPage = { page, value: loadPage(value, page, rowsPerPage) };
+    const newPage = { page, value: loadPage(value, page, itemsPerPage) };
     cache.current.push(newPage);
     if (cache.length > 2) cache.current.shift();
   }
@@ -48,32 +148,173 @@ const useBufferedPages = (page, value) => {
 
 };
 
-const ScrollColumns = ({ value, column: { pages, expanded, gaps, columns } }) => {
+const ScrollColumns = ({
+  columnsPerPage,
+  value,
+  columns: { page, expanded, paddings, columns }, 
+  children,
+  depth = 0
+}) => {
+  const visibleValueRows = useBufferedPages(page, value, columnsPerPage, loadPage);
+  const visibleRows = useBufferedPages(page, columns, columnsPerPage, loadPage);
+  return (
+    <>
+      {paddings.start && <td style={{ height: paddings.start }} />}
+      {visibleRows.map((row, index) => {
+        const rowValue = visibleValueRows[index];
+        return (
+          <React.Fragment key={index}>
+            {children(rowValue, row.columns)}
+            {expanded ? row.children.map((child, index) => {
+              const curChildValue = rowValue.children[index];
+              return <ScrollColumns key={index} value={curChildValue} rows={child} depth={depth + 1} />
+            }) : null}
+          </React.Fragment>
+        )
+      })}
+      {paddings.end && <td style={{ height: paddings.end }} />}
+    </>
+  );
+};
 
+const ScrollRows = ({
+  rowsPerPage,
+  value,
+  rows: { page, expanded, paddings, rows },
+  loadPage,
+  depth = 0,
+  children
+}) => {
+  const visibleValueRows = useBufferedPages(page, value, rowsPerPage, loadPage);
+  const visibleRows = useBufferedPages(page, rows, rowsPerPage, loadPage);
+  return (
+    <>
+      {paddings.start && <tr style={{ height: paddings.start }} />}
+      {visibleRows.map((row, index) => {
+        const rowValue = visibleValueRows[index];
+        return (
+          <React.Fragment key={index}>
+            {children(rowValue, row.columns)}
+            {expanded ? row.children.map((child, index) => {
+              const curChildValue = rowValue.children[index];
+              return (
+                <ScrollRows key={index} value={curChildValue} rows={child} depth={depth + 1}>
+                  {children}
+                </ScrollRows>
+              )
+            }) : null}
+          </React.Fragment>
+        )
+      })}
+      {paddings.end && <tr style={{ height: paddings.end }} />}
+    </>
+  );
+};
+
+const ScrollItems = ({
+  itemsPerPage,
+  value,
+  meta: { page, expanded, paddings, children: metaChildren },
+  loadPage,
+  depth = 0,
+  renderPadding,
+  children
+}) => {
+  const visibleValue = useBufferedPages(page, value, itemsPerPage, loadPage);
+  const visibleMeta = useBufferedPages(page, metaChildren, itemsPerPage, loadPage);
+  return (
+    <>
+      {paddings.start && renderPadding({ paddingTop: paddings.top })}
+      {visibleMeta.map((metaItem, index) => {
+        const value = visibleValue[index];
+        return (
+          <React.Fragment key={index}>
+            {children(value, depth)}
+            {expanded ? metaChildren.map((child, index) => {
+              const curChildValue = value.children[index];
+              return (
+                <ScrollItems key={index} value={curChildValue} meta={child} depth={depth + 1}>
+                  {children}
+                </ScrollItems>
+              )
+            }) : null}
+          </React.Fragment>
+        )
+      })}
+      {paddings.end && renderPadding({ paddingTop: paddings.bottom })}
+    </>
+  );
+};
+
+const StaticScroller = ({
+  children,
+  scrollTop,
+  scrollLeft,
+  ...props
+}) => {
+
+  const getRowsAndColumns = useCallback((scrollLeft, scrollTop) => {
+    // Calculate object somehow
+    return {
+      rows: {},
+      columns: {},
+      margins: {}
+    }
+  }, []);
+
+  const [state, setState] = useState(() => getRowsAndColumns(scrollLeft, scrollTop));
+
+  const handleScroll = e => {
+    const nextState = getRowsAndColumns(e.scrollLeft, e.scrollTop);
+    setState(nextState);
+  };
+
+  return state ? (
+    <div {...props} onScroll={handleScroll}>
+      {children(state)}
+    </div>
+  ) : null;
 };
 
 const loadPage = (value, page, itemsOnPage) => value.slice(page * itemsOnPage, (page + 1) * itemsOnPage);
 
-const ScrollRows = ({ value, rows: { page, expanded, gaps, rows } }) => {
-  const visibleValueRows = useBufferedPages(page, value);
-  const visibleRows = useBufferedPages(page, rows);
+const TestTable = props => {
+  
+  const value = exampleValue;
+
   return (
-    <>
-      {gaps.start && <tr style={{ height: gaps.start }} />}
-      <tr>
-        {visibleRows.map((row, index) => {
-          const curValue = visibleValueRows[index];
-          return (
-            <React.Fragment key={index}>
-              <tr>
-                <ScrollColumns value={curValue} columns={row.columns} />
-              </tr>
-              {expanded ? <ScrollRows rows={rows.children} /> : null}
-            </React.Fragment>
-          )
-        })}
-      </tr>
-      {gaps.end && <tr style={{ height: gaps.end }} />}
-    </>
-  );
+    <StaticScroller>
+      {({ rows, columns, margins }) => {
+        const nextStyle = {
+          marginTop: margins.top,
+          marginBottom: margins.bottom,
+          marginLeft: margins.left,
+          marginRight: margins.right,
+        };
+        return (
+          <table style={nextStyle}>
+            <tbody>
+              <ScrollItems
+                  itemsPerPage={20}
+                  value={value}
+                  meta={rows}
+                  loadPage={loadPage}
+                  renderPadding={props => <tr {...props} />}>
+                {(value, depth) => (
+                  <tr>
+                    <ScrollItems
+                        itemsPerPage={50}
+                        value={value}
+                        meta={columns}
+                        loadPage={loadPage}
+                        renderPadding={props => <td {...props} />} />
+                  </tr>
+                )}
+              </ScrollItems>
+            </tbody>
+          </table>
+        )
+      }}
+    </StaticScroller>
+  )
 };
