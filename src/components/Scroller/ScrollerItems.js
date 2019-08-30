018a1c788e0106f3c488552336ec11b434efad1a
Vadim Sartakov <contact.vadimsartakov@gmail.com>
2019-08-31 00:12:26 +0500

    Add remarks

--- src/components/Scroller/ScrollerItems.js
+++ src/components/Scroller/ScrollerItems.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+// This component should buffer values
+// and maybe calculate current page?
 const ScrollerItems = ({
   // Visible value
   value,
@@ -13,6 +15,7 @@ const ScrollerItems = ({
         const curMeta = metaChildren[index];
         return (
           <React.Fragment key={index}>
+            {/* add map here since client expecting callback execution per each value */}
             {children(curValue, curMeta, depth)}
             {curMeta.expanded ? (
               <ScrollerItems key={index} value={curValue.children} meta={curMeta} depth={depth + 1}>
