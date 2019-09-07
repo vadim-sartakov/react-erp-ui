f46544679521862cd56cd52e1dbb98068b392fdd
Vadim Sartakov <contact.vadimsartakov@gmail.com>
2019-09-08 00:49:34 +0500

    Extend examples

--- src/stories/scroller/scroller.js
+++ src/stories/scroller/scroller.js
@@ -19,7 +19,16 @@ storiesOf('Scroller', module)
     const rows = { children: [] };
     const value = generateValues(columns, 1000);
     value[100].children = generateValues(columns, 200);
-    rows.children[100] = { expanded: true };
+    rows.children[100] = { expanded: true, children: [] };
+
+    value[100].children[50].children = generateValues(columns, 100);
+    rows.children[100].children[50] = { expanded: true, children: [] };
+
+    value[100].children[50].children[1].children = generateValues(columns, 1);
+    rows.children[100].children[50].children[1] = { expanded: true, children: [] };
+
+    value[100].children[50].children[1].children[0].children = generateValues(columns, 1);
+    rows.children[100].children[50].children[1].children[0] = { expanded: true, children: [] };
     return (
       <Table
           rows={rows}
