6393f86c0777cf09a5182a346058dfc8fcc95ce6
Vadim Sartakov <contact.vadimsartakov@gmail.com>
2019-10-19 14:50:44 +0500

    Fixed cell for groups calculation

--- src/components/Spreadsheet/utils.js
+++ src/components/Spreadsheet/utils.js
@@ -1,7 +1,20 @@
 export const getFixedCellOffset = ({ meta, defaultSize, index }) => {
+  const sourceMeta = meta[index];
+  const lastGroupItems = [];
   return [...new Array(index).keys()].reduce((acc, curKey, curIndex) => {
     const curMeta = meta[curIndex];
-    const curResult = curMeta ? (curMeta.size || defaultSize) : defaultSize;
+    let curResult = 0;
+    if (curMeta) {
+      if (curMeta.level) lastGroupItems[curMeta.level - 1] = curMeta;
+      if (curMeta.fixed) {
+        curResult = curMeta.size || defaultSize;
+      }
+      else if (curMeta.isGroup) {
+        curResult = lastGroupItems
+            .slice(0, sourceMeta.level - 1)
+            .reduce((acc, lastGroupMetaItem) => acc + lastGroupMetaItem.size, curResult);
+      }
+    }
     return acc + curResult;
   }, 0);
 };
--- src/components/Spreadsheet/utils.test.js
+++ src/components/Spreadsheet/utils.test.js
@@ -4,18 +4,37 @@ describe('Spreadsheet utils', () => {
 
   describe('getFixedCellOffset', () => {
 
-    it('should calculate offsets for multiple levels', () => {
+    it('should calculate offset for fixed item', () => {
       const defaultSize = 20;
       const meta = [
-        { size: 30 },
-        undefined,
-        { level: 1, size: 50 },
-        { level: 1 },
-        { level: 2 },
+        { size: 30, fixed: true },
+        { fixed: true },
+        { level: 1, size: 50, fixed: true },
+        { level: 1, fixed: true },
+        { level: 2, fixed: true },
         { level: 1 }
       ];
       expect(getFixedCellOffset({ meta, defaultSize, index: 4 })).toEqual(120);
+    });
 
+    it('should calculate offset for group items', () => {
+      const defaultSize = 20;
+      const meta = [
+        { size: 30, fixed: true },
+        { fixed: true },
+        { level: 1, size: 50 },
+        { level: 1, isGroup: true, size: 30 },
+        { level: 2 },
+        { level: 2, isGroup: true },
+        { level: 3 },
+        { level: 3 },
+        { level: 1, isGroup: true },
+        { level: 2 },
+        { level: 2 }
+      ];
+      expect(getFixedCellOffset({ meta, defaultSize, index: 3 })).toEqual(50);
+      expect(getFixedCellOffset({ meta, defaultSize, index: 5 })).toEqual(80);
+      expect(getFixedCellOffset({ meta, defaultSize, index: 8 })).toEqual(50);
     });
 
   });
