11657e030d2e134c6499bfc836efc4a53935382a
Vadim Sartakov <contact.vadimsartakov@gmail.com>
2019-10-25 15:38:28 +0500

    Extend sync grid tests

--- src/components/Scroller/useScroller.test.js
+++ src/components/Scroller/useScroller.test.js
@@ -2,16 +2,13 @@ import { act } from 'react-dom/test-utils';
 import { shallow, mount } from 'enzyme';
 import {
   value,
-  syncGridWithDefaultSizes
+  syncGridWithDefaultSizes,
+  syncGridWithCustomSizes
 } from './useScroller.stories';
 import { loadPage } from './utils';
 
-export const loadRowsPageSync = (page, itemsPerPage) => {
-  return loadPage(value, page, itemsPerPage);
-};
-export const loadRowsPageAsync = (page, itemsPerPage) => {
-  return new Promise(resolve => resolve(loadPage(value, page, itemsPerPage)));
-};
+export const loadRowsPageSync = (page, itemsPerPage) => loadPage(value, page, itemsPerPage);
+export const loadRowsPageAsync = async (page, itemsPerPage) => loadPage(value, page, itemsPerPage);
 
 describe('useScroller', () => {
 
@@ -57,6 +54,47 @@ describe('useScroller', () => {
         expect(wrapper.find('.cover').prop('style').width).toBe(7500);
         expect(wrapper.find('.pages').prop('style').top).toBe(38400);
         expect(wrapper.find('.pages').prop('style').left).toBe(4500);
+        expect(loadRowsPage).toHaveBeenCalledTimes(4);
+      });
+
+      it('should load first rows and columns pages with custom sizes', () => {
+        const loadRowsPage = jest.fn(loadRowsPageSync);
+        const wrapper = shallow(syncGridWithCustomSizes({ loadRowsPage }));
+        expect(wrapper.find('.row').length).toBe(60);
+        expect(wrapper.find('.cell').first().text()).toBe("Value 0 - 0");
+        expect(wrapper.find('.cell').last().text()).toBe("Value 59 - 19");
+        expect(wrapper.find('.cover').prop('style').height).toBe(60000);
+        expect(wrapper.find('.cover').prop('style').width).toBe(9000);
+        expect(wrapper.find('.pages').prop('style').top).toBe(0);
+        expect(wrapper.find('.pages').prop('style').left).toBe(0);
+        expect(loadRowsPage).toHaveBeenCalledTimes(2);
+      });
+
+      it('should load middle rows and columns pages with custom sizes', () => {
+        const loadRowsPage = jest.fn(loadRowsPageSync);
+        const wrapper = shallow(syncGridWithCustomSizes({ loadRowsPage }));
+        wrapper.find('.scroller-container').simulate('scroll', { target: { scrollTop: 3600, scrollLeft: 3600 } })
+        expect(wrapper.find('.row').length).toBe(60);
+        expect(wrapper.find('.cell').first().text()).toBe("Value 30 - 10");
+        expect(wrapper.find('.cell').last().text()).toBe("Value 89 - 29");
+        expect(wrapper.find('.cover').prop('style').height).toBe(60000);
+        expect(wrapper.find('.cover').prop('style').width).toBe(9000);
+        expect(wrapper.find('.pages').prop('style').top).toBe(1800);
+        expect(wrapper.find('.pages').prop('style').left).toBe(1800);
+        expect(loadRowsPage).toHaveBeenCalledTimes(3);
+      });
+
+      it('should load last rows and columns pages with cusrom sizes', () => {
+        const loadRowsPage = jest.fn(loadRowsPageSync);
+        const wrapper = shallow(syncGridWithCustomSizes({ loadRowsPage }));
+        wrapper.find('.scroller-container').simulate('scroll', { target: { scrollTop: 59000, scrollLeft: 8000 } })
+        expect(wrapper.find('.row').length).toBe(40);
+        expect(wrapper.find('.cell').first().text()).toBe("Value 960 - 30");
+        expect(wrapper.find('.cell').last().text()).toBe("Value 999 - 49");
+        expect(wrapper.find('.cover').prop('style').height).toBe(60000);
+        expect(wrapper.find('.cover').prop('style').width).toBe(9000);
+        expect(wrapper.find('.pages').prop('style').top).toBe(57600);
+        expect(wrapper.find('.pages').prop('style').left).toBe(5400);
         expect(loadRowsPage).toHaveBeenCalledTimes(4);
       });
 
