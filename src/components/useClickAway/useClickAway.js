51d9dfd7ca08efd21c68ed257896ceb0bbb696f6
Vadim Sartakov <contact.vadimsartakov@gmail.com>
2019-10-30 17:23:48 +0500

    Improve click away hook

--- src/components/useClickAway/useClickAway.js
+++ src/components/useClickAway/useClickAway.js
@@ -1,19 +1,31 @@
-import { useEffect, useCallback } from 'react';
+import { useRef, useEffect, useCallback } from 'react';
 
 const useClickAway = (ref, onClick) => {
   
+  const initialNode = useRef();
+
+  const handleMouseDown = useCallback(e => {
+    if (e.button === 0) initialNode.current = e.target;
+  }, []);
+
   const handleMouseUp = useCallback(e => {
-    if (e.button === 0 && ref.current && !ref.current.contains(e.target)) {
+    if (e.button === 0 &&
+        ref.current &&
+        initialNode.current && 
+        initialNode.current.isEqualNode(e.target) &&
+        !ref.current.contains(e.target)) {
       onClick(e);
     }
   }, [ref, onClick]);
 
   useEffect(() => {
+    document.addEventListener('mousedown', handleMouseDown);
     document.addEventListener('mouseup', handleMouseUp);
     return () => {
+      document.removeEventListener('mousedown', handleMouseDown);
       document.removeEventListener('mouseup', handleMouseUp);
     };
-  }, [handleMouseUp]);
+  }, [handleMouseDown, handleMouseUp]);
 
 };
 
--- src/components/useClickAway/useClickAway.test.js
+++ src/components/useClickAway/useClickAway.test.js
@@ -18,18 +18,36 @@ const TestComponent = ({ onClickAway }) => {
 
 describe('useClickAway', () => {
   
-  it('should trigger when clicked outside ', () => {
+  it('should trigger click outside when mouse pressed and released on the same item', () => {
     const callback = jest.fn();
     const wrapper = mount(<TestComponent onClickAway={callback} />);
     Object.defineProperty(MouseEvent.prototype, 'target', {
       value: ReactDOM.findDOMNode(wrapper.find('.outside-item').instance()),
       configurable: true
     });
+    document.dispatchEvent(new MouseEvent('mousedown', { button: 0 }));
     document.dispatchEvent(new MouseEvent('mouseup', { button: 0 }));
     expect(callback.mock.calls.length).toBe(1);
   });
 
-  it('should not trigger when ref node clicked', () => {
+  it('should not trigger click outside when mouse pressed on one item and released on another', () => {
+    const callback = jest.fn();
+    const wrapper = mount(<TestComponent onClickAway={callback} />);
+    Object.defineProperty(MouseEvent.prototype, 'target', {
+      value: ReactDOM.findDOMNode(wrapper.find('.nested-item').instance()),
+      configurable: true
+    });
+    document.dispatchEvent(new MouseEvent('mousedown', { button: 0 }));
+
+    Object.defineProperty(MouseEvent.prototype, 'target', {
+      value: ReactDOM.findDOMNode(wrapper.find('.outside-item').instance()),
+      configurable: true
+    });
+    document.dispatchEvent(new MouseEvent('mouseup', { button: 0 }));
+    expect(callback.mock.calls.length).toBe(0);
+  });
+
+  it('should not trigger click outside when ref node clicked', () => {
     const callback = jest.fn();
     const wrapper = mount(<TestComponent onClickAway={callback} />);
     Object.defineProperty(MouseEvent.prototype, 'target', {
