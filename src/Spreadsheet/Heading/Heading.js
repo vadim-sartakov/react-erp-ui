943de203b80f61d0dfb8cdaccbdb803fa7d3c8da
Vadim Sartakov <contact.vadimsartakov@gmail.com>
2020-02-08 15:10:35 +0500

    Memorize heading

--- src/Spreadsheet/Heading/Heading.js
+++ src/Spreadsheet/Heading/Heading.js
@@ -2,22 +2,20 @@ import React, { useCallback } from 'react';
 import HeadingView from './HeadingView';
 import { normalizeMergedRange } from '../../MergedCell';
 
-const Heading = ({
+const Heading = React.memo(({
   Component = HeadingView,
-  selectedCells,
   onResizeInteractionChange,
   ...props
 }) => {
-  const { type, index } = props;
-  const selected = selectedCells.some(selectedRange => {
+  const selected = props.selectedCells.some(selectedRange => {
     const normalizedSelectedRange = normalizeMergedRange(selectedRange);
-    return index >= normalizedSelectedRange.start[type] && index <= normalizedSelectedRange.end[type]
+    return props.index >= normalizedSelectedRange.start[props.type] && props.index <= normalizedSelectedRange.end[props.type]
   });
 
   const onMouseDown = useCallback(() => onResizeInteractionChange({ index: props.index, type: props.type }), [onResizeInteractionChange, props.index, props.type]);
   const onMouseUp = useCallback(() => onResizeInteractionChange(undefined), [onResizeInteractionChange]);
 
   return <Component {...props} selected={selected} onMouseDown={onMouseDown} onMouseUp={onMouseUp} />
-};
+});
 
 export default Heading;
