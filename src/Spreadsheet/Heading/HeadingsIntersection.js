6f808f970923f50e0f488d83db029b76574903cc
Vadim Sartakov <contact.vadimsartakov@gmail.com>
2020-02-13 15:42:02 +0500

    Default value props, update styles

--- src/Spreadsheet/Heading/HeadingsIntersection.js
+++ src/Spreadsheet/Heading/HeadingsIntersection.js
@@ -2,7 +2,7 @@ import React from 'react';
 import SpreadsheetCell from '../SpreadsheetCell';
 
 const HeadingsIntersection = ({ row, column }) => {
-  return <SpreadsheetCell className="heading-cell" row={row} column={column} />
+  return <SpreadsheetCell className="heading-cell heading-intersection" row={row} column={column} />
 };
 
 export default HeadingsIntersection;
