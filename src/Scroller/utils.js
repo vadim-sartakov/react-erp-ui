8564d7635a98828e1ae3e19a269057e40e0d4163
Vadim Sartakov <contact.vadimsartakov@gmail.com>
2020-02-28 00:00:23 +0500

    Start refactoring getIndexesWithCustomSizes

--- src/Scroller/utils.js
+++ src/Scroller/utils.js
@@ -1,22 +1,21 @@
 import { getCellsRangeSize, getCellPosition } from "../grid/MergedCell/utils";
 
 /**
- * @typedef ScrollPage
- * @property {number} start
- * @property {number} end
+ * @typedef Meta
+ * @property {number} size
  */
 
-export const getVisiblePages = page => page === 0 ? [0, 1] : [page - 1, page];
-export const getTotalPages = (totalCount, itemsPerPage) => Math.ceil(totalCount / itemsPerPage);
+export function getTotalPages(totalCount, itemsPerPage){
+  return Math.ceil(totalCount / itemsPerPage);
+};
 
 /**
- * @function
  * @param {number} page 
  * @param {number} itemsPerPage 
  * @param {number} totalCount
  * @returns {number}
  */
-export const getItemsCountOnPage = (page, itemsPerPage, totalCount) => {
+export function getItemsCountOnPage(page, itemsPerPage, totalCount) {
   if (page === undefined) return 0;
   const totalPages = getTotalPages(totalCount, itemsPerPage);
   if (page >= totalPages) return 0;
@@ -24,72 +23,29 @@ export const getItemsCountOnPage = (page, itemsPerPage, totalCount) => {
 };
 
 /**
- * Creates scroll page object structure.
- * It helps to find current page depending on scroll position
- * @function
  * @param {Object} options
- * @param {Meta[]} options.meta [Meta]{@link module:components/Scroller~Meta}
- * @param {number} options.defaultSize 
- * @param {number} options.itemsPerPage 
- * @returns {ScrollPage[]} [ScrollPage]{@link module:components/Scroller/utils~ScrollPage}
+ * @param {number} containerSize
+ * @param {number[]} options.sizes
+ * @param {number} options.scroll
+ * @param {number} options.defaultSize
+ * @param {number} options.totalCount
  */
-export const getScrollPages = ({ meta, totalCount, defaultSize, itemsPerPage }) => {
-  const values = [...new Array(totalCount).keys()];
-  const result = values.reduce(({ curPage, pages }, arrayItem, index, values) => {
-    const curMeta = meta && meta[index];
-
-    const selfSize = ( curMeta && curMeta.size ) || defaultSize;
-    const isNextPage = index > 0 && index % itemsPerPage === 0;
-    
-    let nextPages = isNextPage ? [...pages, curPage] : pages;
-    const nextCurPage = {
-      ...curPage,
-      end: curPage.end + selfSize
-    };
-    if (isNextPage) {
-      nextCurPage.start = curPage.end;
-      delete nextCurPage.children
+export function getVisibleIndexesWithCustomSizes({ containerSize, sizes, scroll, defaultSize, totalCount, overscroll = 0 }) {
+  let curScroll = 0;
+  let firstIndex, lastIndex;
+  for (let curIndex = 0; curIndex < totalCount; curIndex++) {
+    curScroll += sizes[curIndex] || defaultSize;
+    if (firstIndex === undefined && curScroll >= scroll) firstIndex = curIndex;
+    if (lastIndex === undefined && curScroll >= (scroll + containerSize)) {
+      lastIndex = curIndex;
+      break;
     }
-    
-    if (index === values.length - 1) nextPages = [...nextPages, nextCurPage];
-    return {
-      curPage: nextCurPage,
-      pages: nextPages
-    }
-  }, {
-    curPage: { start: 0, end: 0 },
-    pages: []
-  });
-  return result.pages;
-};
-
-/**
- * @function
- * @param {ScrollPage[]} scrollPages [ScrollPage]{@link module:components/Scroller/utils~ScrollPage}
- * @param {number} [scroll=0]
- * @returns {number}
- */
-export const getPageNumberFromScrollPages = (scrollPages, scroll = 0) => {
-  if (scroll < 0) return 0;
-
-  const lastPageIndex = scrollPages.length - 1;
-  if (scroll > scrollPages[lastPageIndex].end) return lastPageIndex;
-
-  const currentPage = scrollPages.reduce((acc, page, index) => {
-    if (acc.pageIndex !== -1) return acc;
-    
-    const pageSize = page.end - page.start;
-    const pageHalf = pageSize / 2;
-    const curScroll = acc.curScroll + pageSize;
-
-    const isInRange = scroll >= page.start && scroll < page.end;
-    if (!isInRange) return { ...acc, curScroll };
-
-    const pageIndex = scroll > (page.start + pageHalf) ? index + 1 : index;
-
-    return { curScroll, pageIndex };
-  }, { curScroll: 0, pageIndex: -1 });
-  return currentPage.pageIndex;
+  }
+  firstIndex = Math.max(firstIndex - overscroll, 0);
+  lastIndex = Math.min(lastIndex + overscroll, totalCount - 1);
+  const result = [];
+  for (let i = firstIndex; i <= lastIndex; i++) result.push(i);
+  return result;
 };
 
 /**
@@ -101,7 +57,7 @@ export const getPageNumberFromScrollPages = (scrollPages, scroll = 0) => {
  * @param {number} options.overscroll
  * @returns {number} 
  */
-export function getVisibleIndexesWithDefaultSize({ containerSize, defaultSize, totalCount, scroll, overscroll = 0 }) {
+export function getVisibleIndexesWithDefaultSizes({ containerSize, defaultSize, totalCount, scroll, overscroll = 0 }) {
   const visibleElementsCount = Math.ceil(containerSize / defaultSize);
   const maxIndex = totalCount - 1;
   let firstIndex = Math.max(Math.floor(scroll / defaultSize), 0);
@@ -114,25 +70,26 @@ export function getVisibleIndexesWithDefaultSize({ containerSize, defaultSize, t
 };
 
 /**
- * Generic page number calculation function which decides how page number
+ * Generic visible indexes calculation function which decides how indexes
  * will be calculated depending on whether meta option is specified or not
  * @function
  * @param {Object} options
  * @param {ScrollPage} options.scrollPages
  * @param {Meta[]} options.meta
  * @param {number} options.defaultSize
- * @param {number} options.itemsPerPage
+ * @param {number} options.containerSize
  * @param {number} options.totalCount
  * @param {number} options.scroll
+ * @param {number} options.overscroll
  */
-export const getPageNumber = ({ scrollPages, defaultSize, itemsPerPage, totalCount, scroll }) => {
-  let curPage;
+export const getVisibleIndexes = ({ scrollPages, defaultSize, totalCount, containerSize, scroll, overscroll = 0 }) => {
+  let visibleIndexes;
   if (scrollPages && scrollPages.length) {
-    curPage = getPageNumberFromScrollPages(scrollPages, scroll);
+    visibleIndexes = getVisibleIndexesWithCustomSizes(scrollPages, scroll);
   } else {
-    curPage = getPageNumberWithDefaultSize({ defaultSize, itemsPerPage, totalCount, scroll });
+    visibleIndexes = getVisibleIndexesWithDefaultSizes({ defaultSize, containerSize, totalCount, scroll, overscroll });
   }
-  return curPage;
+  return visibleIndexes;
 };
 
 /**
--- src/Scroller/utils.test.js
+++ src/Scroller/utils.test.js
@@ -1,8 +1,7 @@
 import {
   getItemsCountOnPage,
-  getScrollPages,
-  getPageNumberFromScrollPages,
-  getVisibleIndexesWithDefaultSize,
+  getVisibleIndexesWithDefaultSizes,
+  getVisibleIndexesWithCustomSizes,
   getGapsWithDefaultSize,
   getGapsFromScrollPages,
   getFixedOffsets
@@ -19,140 +18,46 @@ describe('Scroller utils', () => {
     });
   });
 
-  describe('getScrollPages', () => {
-
-    it('should return equal ranges with default sizes and total count 3', () => {
-      const meta = [
-        {},
-        {},
-        {}
-      ];
-      const result = getScrollPages({ meta, totalCount: 3, defaultSize: 20, itemsPerPage: 2 });
-      expect(result).toEqual([
-        { start: 0, end: 40 },
-        { start: 40, end: 60 }
-      ]);
-    });
-
-    it('should return equal ranges with default sizes and total count 4', () => {
-      const meta = [
-        {},
-        {},
-        {},
-        {}
-      ];
-      const result = getScrollPages({ meta, defaultSize: 20, itemsPerPage: 2, totalCount: 4 });
-      expect(result).toEqual([
-        { start: 0, end: 40 },
-        { start: 40, end: 80 }
-      ]);
-    });
-
-    it('should return longer range when custom size in the middle is specified', () => {
-      const meta = [
-        {},
-        { size: 40 },
-        {}
-      ];
-      const result = getScrollPages({ meta, defaultSize: 20, itemsPerPage: 2, totalCount: 3 });
-      expect(result).toEqual([
-        { start: 0, end: 60 },
-        { start: 60, end: 80 }
-      ]);
-    });
-
-    it('should return longer ranges when custom size is specified in multiple places', () => {
-      const meta = [
-        {},
-        { size: 40 },
-        { size: 60 }
-      ];
-      const result = getScrollPages({ meta, defaultSize: 20, itemsPerPage: 2, totalCount: 3 });
-      expect(result).toEqual([
-        { start: 0, end: 60 },
-        { start: 60, end: 120 }
-      ]);
-    });
-
-  });
-
-  describe('getPageNumberFromScrollPages', () => {
-
-    it('should return initial page - 0 on scroll 0', () => {
-      const scrollPages = [
-        { start: 0, end: 20 },
-        { start: 20, end: 40 }
-      ];
-      expect(getPageNumberFromScrollPages(scrollPages, 0)).toBe(0);
-    });
-
-    it('should return cur page when scrolled less then half of current', () => {
-      const scrollPages = [
-        { start: 0, end: 20 },
-        { start: 20, end: 40 },
-        { start: 40, end: 60 },
-        { start: 60, end: 80 },
-      ];
-      expect(getPageNumberFromScrollPages(scrollPages, 45)).toBe(2);
-    });
-
-    it('should return next page when scrolled half of current', () => {
-      const scrollPages = [
-        { start: 0, end: 20 },
-        { start: 20, end: 40 },
-        { start: 40, end: 60 },
-        { start: 60, end: 80 },
-      ];
-      expect(getPageNumberFromScrollPages(scrollPages, 55)).toBe(3);
-    });
-
-    it('should return initial page - 0 on negative scroll', () => {
-      const scrollPages = [
-        { start: 0, end: 20 },
-        { start: 20, end: 40 }
-      ];
-      expect(getPageNumberFromScrollPages(scrollPages, -20)).toBe(0);
-    });
-
-    it('should return last page when next page is more than page count', () => {
-      const scrollPages = [
-        { start: 0, end: 20 },
-        { start: 20, end: 40 }
-      ];
-      expect(getPageNumberFromScrollPages(scrollPages, 200)).toBe(1);
-    });
-
-    it('should return last page when scroll value is more than the whole scroll range size', () => {
-      const scrollPages = [
-        { start: 0, end: 20 },
-        { start: 20, end: 40 }
-      ];
-      expect(getPageNumberFromScrollPages(scrollPages, 200)).toBe(1);
-    });
-  });
-
-  describe('getVisibleIndexesWithDefaultSize', () => {
+  describe('getVisibleIndexesWithDefaultSizes', () => {
     it('should return first visible indexes with default overscroll', () => {
-      expect(getVisibleIndexesWithDefaultSize({ containerSize: 50, defaultSize: 10, scroll: 0, totalCount: 200 })).toEqual([0, 1, 2, 3, 4]);
-    });
-    it('should return first visible indexes on negative scroll value and provided overscroll value', () => {
-      expect(getVisibleIndexesWithDefaultSize({ containerSize: 50, defaultSize: 10, scroll: -150, totalCount: 200, overscroll: 2 })).toEqual([0, 1, 2, 3, 4, 5, 6]);
+      expect(getVisibleIndexesWithDefaultSizes({ containerSize: 50, defaultSize: 10, scroll: 0, totalCount: 200 })).toEqual([0, 1, 2, 3, 4]);
     });
     it('should not get out of start bounds with overscroll value', () => {
-      expect(getVisibleIndexesWithDefaultSize({ containerSize: 50, defaultSize: 10, scroll: 10, totalCount: 200, overscroll: 2 })).toEqual([0, 1, 2, 3, 4, 5, 6, 7]);
+      expect(getVisibleIndexesWithDefaultSizes({ containerSize: 50, defaultSize: 10, scroll: 10, totalCount: 200, overscroll: 2 })).toEqual([0, 1, 2, 3, 4, 5, 6, 7]);
     });
     it('should not get out of end bounds with overscroll value', () => {
-      expect(getVisibleIndexesWithDefaultSize({ containerSize: 50, defaultSize: 10, scroll: 1950, totalCount: 200, overscroll: 2 })).toEqual([193, 194, 195, 196, 197, 198, 199]);
+      expect(getVisibleIndexesWithDefaultSizes({ containerSize: 50, defaultSize: 10, scroll: 1950, totalCount: 200, overscroll: 2 })).toEqual([193, 194, 195, 196, 197, 198, 199]);
     });
     it('should return visible indexes in middle scroll and default overscroll', () => {
-      expect(getVisibleIndexesWithDefaultSize({ containerSize: 50, defaultSize: 10, scroll: 50, totalCount: 200 })).toEqual([5, 6, 7, 8, 9]);
+      expect(getVisibleIndexesWithDefaultSizes({ containerSize: 50, defaultSize: 10, scroll: 50, totalCount: 200 })).toEqual([5, 6, 7, 8, 9]);
     });
     it('should return visible indexes in middle scroll and provided overscroll value', () => {
-      expect(getVisibleIndexesWithDefaultSize({ containerSize: 50, defaultSize: 10, scroll: 50, totalCount: 200, overscroll: 2 })).toEqual([3, 4, 5, 6, 7, 8, 9, 10, 11]);
+      expect(getVisibleIndexesWithDefaultSizes({ containerSize: 50, defaultSize: 10, scroll: 50, totalCount: 200, overscroll: 2 })).toEqual([3, 4, 5, 6, 7, 8, 9, 10, 11]);
     });
   });
 
-  describe('getGapsWithDefaultSize', () => {
+  describe('getVisibleIndexesWithCustomSizes', () => {
+
+    const sizes = [20, 30, 50, 80, 20, 10, 50, 90, 40, 30];
+
+    it('should return first visible indexes', () => {
+      const result = getVisibleIndexesWithCustomSizes({ sizes, containerSize: 50, defaultSize: 10, totalCount: 200, scroll: 0 });
+      expect(result).toEqual([0, 1]);
+    });
+
+    it('should return first extended visible indexes when overscroll specified', () => {
+      const result = getVisibleIndexesWithCustomSizes({ sizes, containerSize: 50, defaultSize: 10, totalCount: 200, scroll: 0, overscroll: 2 });
+      expect(result).toEqual([0, 1, 2, 3]);
+    });
+
+    it('should return middle visible indexes when scrolled', () => {
+      const result = getVisibleIndexesWithCustomSizes({ sizes, containerSize: 50, defaultSize: 10, totalCount: 200, scroll: 130 });
+      expect(result).toEqual([4, 5, 6]);
+    });
+
+  });
+
+  describe.skip('getGapsWithDefaultSize', () => {
     it('start page', () => {
       expect(getGapsWithDefaultSize({ defaultSize: 20, itemsPerPage: 1, totalCount: 5, page: 0 })).toEqual({ start: 0, middle: 40, end: 60 });
     });
@@ -170,7 +75,7 @@ describe('Scroller utils', () => {
     });
   });
 
-  describe('getGapsFromScrollPages', () => {
+  describe.skip('getGapsFromScrollPages', () => {
     const scrollPages = [
       { start: 0, end: 20 },
       { start: 20, end: 60 },
